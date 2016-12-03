ee6ad23ebc34fe9a6b0573236981a6e1e2e2713b
Iru Cai <mytbk920423@gmail.com>
2016-12-03 19:41:15 +0800

    a maybe better param set, simplify the code

--- train.py
+++ train.py
@@ -1,31 +1,34 @@
 #!/usr/bin/env python2
 
 import xgboost as xgb
+from numpy import array
 dtrain = xgb.DMatrix('out.txt.train')
 dval = xgb.DMatrix('out.txt.validate')
 dtest = xgb.DMatrix('out.txt.test')
-param = { 'max_depth': 3,
-          'eta': 0.05,
+param = { 'max_depth': 4,
+          'eta': 0.03,
           'gamma': 1.0,
           'min_child_weight': 1,
           'save_period': 0,
           'booster': 'gbtree',
-          'subsample': 0.7,
+          'subsample': 0.6,
           'colsample_bytree': 0.7,
           'eval_metric': 'rmse',
-          'nthreads': 2,
+          'nthreads': 4,
           'objective': 'reg:tweedie' }
-num_round = 500
+num_round = 600
 watchlist = [(dval, 'eval'), (dtrain, 'train')]
 bst = xgb.train(param, dtrain, num_round, watchlist)
 bst.dump_model('model.txt')
 
 preds = bst.predict(dtest)
 predy = []
+predyint = []
 outfile = open('result.txt', 'w')
 for i in preds:
     predy.append(i)
-    outfile.write('%f\n' % i)
+    predyint.append(int(round(i)))
+    outfile.write('%d\n' % int(round(i)))
 
 outfile.close()
 
@@ -34,10 +37,5 @@ testy = []
 for i in tf:
     testy.append(int(i.split()[0]))
 
-print('%d %d\n'%(len(predy),len(testy)))
-rmse = 0.0
-for i in range(0, len(predy)):
-    rmse += (predy[i]-testy[i])**2
-
-rmse = (rmse/len(predy))**0.5
-print('rmse=%f'%rmse)
+print("rmse(float): %f" % (array(predy)-array(testy)).std())
+print("rmse(int): %f" % (array(predyint)-array(testy)).std())
