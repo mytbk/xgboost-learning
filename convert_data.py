a47de91d45246255faf75f0a9567a74c230eb9f4
Iru Cai <mytbk920423@gmail.com>
2016-12-03 16:20:54 +0800

    make a separate getDataFromCSV procedure

--- convert_data.py
+++ convert_data.py
@@ -4,31 +4,31 @@ import pandas
 import sys
 from types import *
 
-table = pandas.read_csv(sys.argv[1])
-column_names = table.columns
-working = pandas.DataFrame()
+def getDataFromCSV(csv):
+        table = pandas.read_csv(csv)
+        column_names = table.columns
+        working = pandas.DataFrame()
 
-for col in column_names:
-	print(col)
-	if type(table[col][0]) is not StringType:
-		print('int')
-		working = pandas.concat([working, table[col]], axis=1)
-	else:
-		print('str')
-		dummy = pandas.get_dummies(table[col], prefix=col)
-		working = pandas.concat([working, dummy], axis=1)
+        for col in column_names:
+	        print(col)
+	        if type(table[col][0]) is not StringType:
+		        working = pandas.concat([working, table[col]], axis=1)
+	        else:
+		        dummy = pandas.get_dummies(table[col], prefix=col)
+		        working = pandas.concat([working, dummy], axis=1)
 
-#print(table)
-#print(working)
+        return working
 
-fo = open(sys.argv[2], 'w')
-for row in working.values:
-	fo.write('%d' % row[1])
-	data = row[2:]
-	for i in range(0,len(data)):
-		if data[i] != 0:
-			fo.write(' %d:%d' % (i+1, data[i]))
-	fo.write('\n')
+if __name__ == '__main__':
+        working = getDataFromCSV(sys.argv[1])
+        fo = open(sys.argv[2], 'w')
+        for row in working.values:
+	        fo.write('%d' % row[1])
+	        data = row[2:]
+	        for i in range(0,len(data)):
+		        if data[i] != 0:
+			        fo.write(' %d:%d' % (i+1, data[i]))
+	        fo.write('\n')
 
-fo.close()
+        fo.close()
 
